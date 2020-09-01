289a4229b1c904393af6cdfd42f9e813a29985bb
Chocohead <Chocohead@Aroma1997.org>
2020-09-02 00:32:33 +0100

    Fix custom fluid colours
    Fixes #15

---
+++ src/main/java/me/modmuss50/optifabric/compat/fabricrenderingfluids/FluidRendererFix.java
@@ -0,0 +1,96 @@
+package me.modmuss50.optifabric.compat.fabricrenderingfluids;
+
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.IntInsnNode;
+import org.objectweb.asm.tree.JumpInsnNode;
+import org.objectweb.asm.tree.LabelNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.VarInsnNode;
+
+import me.modmuss50.optifabric.patcher.fixes.ClassFixer;
+import me.modmuss50.optifabric.util.RemappingUtils;
+
+public class FluidRendererFix implements ClassFixer {
+	//Add a little decoy so Fabric injects a little earlier, then patch the result
+
+	@Override
+	public void fix(ClassNode optifine, ClassNode minecraft) {
+		//(BlockRenderView, BlockPos, VertexConsumer, FluidState)
+		String renderDesc = "(Lnet/minecraft/class_1920;Lnet/minecraft/class_2338;Lnet/minecraft/class_4588;Lnet/minecraft/class_3610;)Z";
+		//FluidRenderer, render, (..^..)
+		String render = RemappingUtils.getMethodName("class_775", "method_3347", renderDesc);
+		renderDesc = RemappingUtils.mapMethodDescriptor(renderDesc);
+
+		for (MethodNode method : optifine.methods) {
+			if (render.equals(method.name) && renderDesc.equals(method.desc)) {
+				JumpInsnNode setTint = null;
+
+				for (AbstractInsnNode node : method.instructions) {
+					if (node.getType() == AbstractInsnNode.METHOD_INSN) {
+						MethodInsnNode methodInsn = (MethodInsnNode) node;
+
+						if ("net/optifine/CustomColors".equals(methodInsn.owner) && "getFluidColor".equals(methodInsn.name)) {
+							do {
+								node = node.getPrevious();
+							} while (node != null && node.getType() != AbstractInsnNode.JUMP_INSN);
+							if (node == null) throw new IllegalStateException("Unable to find injection point in " + optifine.name + '#' + method.name + method.desc);
+
+							setTint = (JumpInsnNode) node;
+							break;
+						}
+					}
+				}
+
+				//If setTint is still null the CustomColors#getFluidColor call couldn't be found in the method body
+				if (setTint == null) throw new IllegalStateException("Unable to find injection point in " + optifine.name + '#' + method.name + method.desc);
+
+				LabelNode needsOptiFine = new LabelNode();
+				//LabelNode noNeedPop = new LabelNode();
+				LabelNode noNeed = setTint.label;
+
+				InsnList extra = new InsnList();
+				extra.add(new IntInsnNode(Opcodes.BIPUSH, 16));
+				extra.add(new InsnNode(Opcodes.POP));
+				method.instructions.insertBefore(setTint, extra);
+
+				setTint.label = needsOptiFine;
+				setTint.setOpcode(Opcodes.IFLT);
+
+				extra.add(new VarInsnNode(Opcodes.ALOAD, 4));
+				/*Member getFluid = RemappingUtils.mapMethod("class_3610", "method_15772", "()Lnet/minecraft/class_3611;");
+				extra.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, getFluid.owner, getFluid.name, getFluid.desc));
+				extra.add(new InsnNode(Opcodes.DUP));
+				Member water = RemappingUtils.mapMethod("class_3612", "field_15910", "Lnet/minecraft/class_3609;");
+				extra.add(new FieldInsnNode(Opcodes.GETSTATIC, water.owner, water.name, water.desc));
+				extra.add(new JumpInsnNode(Opcodes.IF_ACMPNE, noNeedPop));
+				extra.add(new MethodInsnNode(Opcodes.GETSTATIC, "net/fabricmc/fabric/api/client/render/fluid/v1/FluidRenderHandlerRegistry", "INSTANCE",
+						"Lnet/fabricmc/fabric/api/client/render/fluid/v1/FluidRenderHandlerRegistry;"));
+				extra.add(new InsnNode(Opcodes.SWAP));
+				extra.add(new MethodInsnNode(Opcodes.INVOKEINTERFACE, "net/fabricmc/fabric/api/client/render/fluid/v1/FluidRenderHandlerRegistry", "get",
+						'(' + water.desc + ")Lnet/fabricmc/fabric/api/client/render/fluid/v1/FluidRenderHandler;", true));
+				extra.add(new LdcInsnNode("Can't find render handler for water?"));
+				extra.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "java/util/Objects", "requireNonNull", "(Ljava/lang/Object;Ljava/lang/String;)Ljava/lang/Object;", false));
+				extra.add(new TypeInsnNode(Opcodes.CHECKCAST, "net/fabricmc/fabric/api/client/render/fluid/v1/FluidRenderHandler")); //We'll be good
+				extra.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "java/lang/Object", "getClass", "()Ljava/lang/Class;", false));
+				extra.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "java/lang/Class", "getName", "()Ljava/lang/String;", false));
+				extra.add(new LdcInsnNode("net.fabricmc.fabric."));
+				extra.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "java/lang/String", "startsWith", "(Ljava/lang/String;)Z", false));*/
+				extra.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "me/modmuss50/optifabric/compat/fabricrenderingfluids/FluidRendererFixExternal", 
+						"needsOptiFine", RemappingUtils.mapMethodDescriptor("(Lnet/minecraft/class_3610;)Z"))); //FluidState
+				extra.add(new JumpInsnNode(Opcodes.IFEQ, noNeed));
+				extra.add(needsOptiFine);
+				method.instructions.insert(setTint, extra);/*
+
+				extra.add(noNeedPop);
+				extra.add(new InsnNode(Opcodes.POP));
+				method.instructions.insertBefore(noNeed, extra);*/
+				break;
+			}
+		}
+	}
+}
---
+++ src/main/java/me/modmuss50/optifabric/compat/fabricrenderingfluids/FluidRendererFixExternal.java
@@ -0,0 +1,20 @@
+package me.modmuss50.optifabric.compat.fabricrenderingfluids;
+
+import java.util.Objects;
+
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
+
+/** Called from {@link FluidRendererFix}, here to avoid class loading Minecraft stuff too early */
+public class FluidRendererFixExternal {
+	public static boolean needsOptiFine(FluidState state) {
+		return needsOptiFine(state.getFluid());
+	}
+
+	public static boolean needsOptiFine(Fluid fluid) {
+		return fluid == Fluids.WATER && Objects.requireNonNull(FluidRenderHandlerRegistry.INSTANCE.get(fluid), "Can't find render handler for water?").getClass().getName().startsWith("net.fabricmc.fabric.");
+	}
+}
