6a49a174eb73202a900e771f287392e47ea1b6ec
Chocohead <Chocohead@Aroma1997.org>
2020-09-20 22:45:14 +0100

    Avoid creating unique lambdas for each patch
    They're all the same so reusing will save a small amount of memory

--- src/main/java/me/modmuss50/optifabric/mod/OptifineInjector.java
+++ src/main/java/me/modmuss50/optifabric/mod/OptifineInjector.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Modifier;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Consumer;
 
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
@@ -36,63 +37,65 @@ public class OptifineInjector {
 	}
 
 	public void setup() {
-		for (String name : classCache.getClasses()) {
-			ClassTinkerers.addReplacement(name, target -> {
-				//Avoid double patching things, not that this should happen
-				if (!patched.add(target.name)) {
-					System.out.println("Already patched " + target.name);
-					return;
-				}
+		Consumer<ClassNode> transformer = target -> {
+			//Avoid double patching things, not that this should happen
+			if (!patched.add(target.name)) {
+				System.out.println("Already patched " + target.name);
+				return;
+			}
 
-				//Skip applying class patches we veto
-				if (OptifineFixer.INSTANCE.shouldSkip(target.name)) {
-					return;
-				}
+			//Skip applying class patches we veto
+			if (OptifineFixer.INSTANCE.shouldSkip(target.name)) {
+				return;
+			}
 
-				//Remember the access we started with
-				Object2IntMap<String> memberToAccess = new Object2IntArrayMap<>(target.methods.size());
-				memberToAccess.defaultReturnValue(-1);
-				for (MethodNode method : target.methods) {
-					memberToAccess.put(method.name + method.desc, method.access);
-				}
-				for (FieldNode field : target.fields) {
-					memberToAccess.put(field.name + ' ' + field.desc, field.access);
-				}
+			//Remember the access we started with
+			Object2IntMap<String> memberToAccess = new Object2IntArrayMap<>(target.methods.size());
+			memberToAccess.defaultReturnValue(-1);
+			for (MethodNode method : target.methods) {
+				memberToAccess.put(method.name + method.desc, method.access);
+			}
+			for (FieldNode field : target.fields) {
+				memberToAccess.put(field.name + ' ' + field.desc, field.access);
+			}
 
-				//I cannot imagine this being very good at all
-				ClassNode source = getSourceClassNode(target);
+			//I cannot imagine this being very good at all
+			ClassNode source = getSourceClassNode(target);
 
-				//Patch the class if required
-				OptifineFixer.INSTANCE.getFixers(target.name).forEach(classFixer -> classFixer.fix(source, target));
+			//Patch the class if required
+			OptifineFixer.INSTANCE.getFixers(target.name).forEach(classFixer -> classFixer.fix(source, target));
 
-				target.methods = source.methods;
-				target.fields = source.fields;
-				target.interfaces = source.interfaces;
-				target.superName = source.superName;
+			target.methods = source.methods;
+			target.fields = source.fields;
+			target.interfaces = source.interfaces;
+			target.superName = source.superName;
 
-				//Classes should be read with frames expanded (as Mixin itself does it), in which case this should all be fine
-				for (MethodNode methodNode : target.methods) {
-					for (AbstractInsnNode insnNode : methodNode.instructions.toArray()) {
-						if (insnNode instanceof FrameNode) {
-							FrameNode frameNode = (FrameNode) insnNode;
-							if (frameNode.local == null) {
-								throw new IllegalStateException("Null locals in " + frameNode.type + " frame @ " + source.name + "#" + methodNode.name + methodNode.desc);
-							}
+			//Classes should be read with frames expanded (as Mixin itself does it), in which case this should all be fine
+			for (MethodNode methodNode : target.methods) {
+				for (AbstractInsnNode insnNode : methodNode.instructions.toArray()) {
+					if (insnNode instanceof FrameNode) {
+						FrameNode frameNode = (FrameNode) insnNode;
+						if (frameNode.local == null) {
+							throw new IllegalStateException("Null locals in " + frameNode.type + " frame @ " + source.name + "#" + methodNode.name + methodNode.desc);
 						}
 					}
 				}
+			}
 
-				// Lets make every class we touch match the access it used to have
-				target.access = widerAccess(target.access, source.access);
-				for (MethodNode method : target.methods) {
-					int access = memberToAccess.getInt(method.name + method.desc);
-					if (access != -1) method.access = widerAccess(access, method.access);
-				}
-				for (FieldNode field : target.fields) {
-					int access = memberToAccess.getInt(field.name + ' ' + field.desc);
-					if (access != -1) field.access = widerAccess(access, field.access);
-				}
-			});
+			// Lets make every class we touch match the access it used to have
+			target.access = widerAccess(target.access, source.access);
+			for (MethodNode method : target.methods) {
+				int access = memberToAccess.getInt(method.name + method.desc);
+				if (access != -1) method.access = widerAccess(access, method.access);
+			}
+			for (FieldNode field : target.fields) {
+				int access = memberToAccess.getInt(field.name + ' ' + field.desc);
+				if (access != -1) field.access = widerAccess(access, field.access);
+			}
+		};
+
+		for (String name : classCache.getClasses()) {
+			ClassTinkerers.addReplacement(name, transformer);
 		}
 	}
 
